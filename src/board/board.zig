6236ac9c143064a96c9a6fa3e1293495e9d4cf47
Arey125 <andrej.kurenckoff@gmail.com>
2024-01-06 18:59:10 +0300

    feat: player roles

--- src/board/board.zig
+++ src/board/board.zig
@@ -7,6 +7,8 @@ var mutex = std.Thread.Mutex{};
 
 var map = [_][10]u8{[_]u8{' '} ** 10} ** 10;
 
+var players = [_]?u32{ null, null };
+
 pub fn init(alloc: std.mem.Allocator) void {
     allocator = alloc;
 }
@@ -15,6 +17,14 @@ pub fn handler(res: *Response) !void {
     mutex.lock();
     defer mutex.unlock();
 
+    if (players[0] == null) {
+        players[0] = res.address.in.sa.addr;
+    } else if (players[1] == null) {
+        players[1] = res.address.in.sa.addr;
+    }
+
+    const mark: u8 = if (players[0] == res.address.in.sa.addr) 'X' else 'O';
+
     if (res.request.method == .POST) {
         const body = res.reader().readAllAlloc(allocator, 8192) catch unreachable;
         defer allocator.free(body);
@@ -22,14 +32,18 @@ pub fn handler(res: *Response) !void {
         const equal_sign_pos = std.mem.indexOf(u8, body, "=") orelse unreachable;
         const col = std.fmt.parseInt(usize, body[equal_sign_pos + 1 .. equal_sign_pos + 2], 10) catch unreachable;
         const row = std.fmt.parseInt(usize, body[equal_sign_pos + 5 .. equal_sign_pos + 6], 10) catch unreachable;
-        std.log.info("{x} placed X at [{}, {}]", .{ res.address.in.sa.addr, row, col });
+        std.log.info("{x} placed {c} at [{}, {}]", .{ res.address.in.sa.addr, mark, row, col });
 
-        map[row][col] = 'X';
+        map[row][col] = mark;
     }
+
+    try res.writer().print(@embedFile("./header.html"), .{mark});
+    try res.writer().writeAll("<div class=\"board-cells\">");
 
     for (map, 0..) |line, row| {
         for (line, 0..) |cell, col| {
-            res.writer().print(@embedFile("./cell.html"), .{ cell, col, row }) catch unreachable;
+            try res.writer().print(@embedFile("./cell.html"), .{ cell, col, row });
         }
     }
+    try res.writer().writeAll("</div>");
 }
